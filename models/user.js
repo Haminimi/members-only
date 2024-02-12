e67f051dc7710fd9c01199751bda199e9bb3f065
Hamza Mujinovic <haminimi.dev@gmail.com>
2024-02-12 19:59:35 +0100

    Add more fields to the user schema

--- models/user.js
+++ models/user.js
@@ -3,8 +3,20 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
+	firstName: { type: String, required: true },
+	lastName: { type: String, required: true },
 	username: { type: String, required: true },
 	password: { type: String, required: true },
+	membershipStatus: {
+		type: String,
+		enum: ['Guest', 'Member', 'Admin'],
+		default: 'Guest',
+		required: true,
+	},
+});
+
+UserSchema.virtual('fullName').get(function () {
+	return `${this.firstName} ${this.lastName}`;
 });
 
 module.exports = mongoose.model('User', UserSchema);
